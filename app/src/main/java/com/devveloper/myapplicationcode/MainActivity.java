37c8444ecfbd0235eb7ca1ab2a98f2393c2dd29c
Abhinav kumar <abhinhavkumar2000@gmail.com>
2022-01-21 02:00:03 +0530

    Update is done in the app

--- app/src/main/java/com/devveloper/myapplicationcode/MainActivity.java
+++ app/src/main/java/com/devveloper/myapplicationcode/MainActivity.java
@@ -2,13 +2,31 @@ package com.devveloper.myapplicationcode;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView my;
+    Button click;
+    @SuppressLint("ResourceAsColor")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        my=findViewById(R.id.firstcheck);
+        my.setText("I am aBHINAV kUMAAR ");
+        my.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                my.setText("Secret Meet");
+            }
+        });
+
     }
 }
