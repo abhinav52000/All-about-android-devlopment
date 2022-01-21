28a7155b59e882373dc6ed9c77a520b4cefb9b02
Abhinav kumar <abhinhavkumar2000@gmail.com>
2022-01-22 00:34:00 +0530

    This commit includes the edit text operation

--- app/src/main/java/com/devveloper/myapplicationcode/MainActivity.java
+++ app/src/main/java/com/devveloper/myapplicationcode/MainActivity.java
@@ -6,27 +6,52 @@ import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView my;
-    Button click;
+    TextView sapna;
+    Button button1;
+    EditText apna;
+    String me="Abhinav",check;
+    int count=0;
     @SuppressLint("ResourceAsColor")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        my=findViewById(R.id.firstcheck);
-        my.setText("I am aBHINAV kUMAAR ");
-        my.setOnClickListener(new View.OnClickListener() {
+        apna=findViewById(R.id.namtera);
+        sapna=findViewById(R.id.jwabmera);
+        button1=findViewById(R.id.checkwla);
+
+        button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                check=apna.getText().toString();
 
-                my.setText("Secret Meet");
+                if(me.equals(check))
+                {
+                    sapna.setText("Permission granted");
+                    count=0;
+                }
+                else
+                {
+                    if (count==5) {
+                        sapna.setText("This IP is blocked");
+                        Thread.Sleep(3000);
+                        System.exit(1);
+                    }
+                    sapna.setText("Try once again 😊"+count);
+                    ++count;
+                }
             }
+
         });
+
+
+
 
     }
 }
