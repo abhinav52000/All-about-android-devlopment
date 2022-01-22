959ded579067a336bb5a9f3ae34ca94cec59d889
Abhinav kumar <abhinhavkumar2000@gmail.com>
2022-01-23 00:29:46 +0530

    This update is containing code for list view example

--- app/src/main/java/com/devveloper/myapplicationcode/MainActivity.java
+++ app/src/main/java/com/devveloper/myapplicationcode/MainActivity.java
@@ -1,73 +1,44 @@
 package com.devveloper.myapplicationcode;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.LinearLayoutCompat;
 
-import android.app.Dialog;
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Spinner;
-import android.widget.TextView;
+import android.widget.ListView;
 import android.widget.Toast;
 
-import com.google.android.material.snackbar.Snackbar;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView result;
-    Spinner spinner;
-    ArrayAdapter adapter;
-    LinearLayoutCompat linear;
+    String countrylist[];
+    ArrayAdapter<String> adapter;
+    ListView listView;
+
                 @Override
                 protected void onCreate(Bundle savedInstanceState) {
                     super.onCreate(savedInstanceState);
                     setContentView(R.layout.activity_main);
 
-
-                    adapter=ArrayAdapter.createFromResource(this, R.array.country,
-                            android.R.layout.simple_spinner_item);
-                    spinner=findViewById(R.id.spinnere);
-                    result=findViewById(R.id.resulte);
-                    linear=findViewById(R.id.layoutme);
-                    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-                    spinner.setAdapter(adapter);
-                    spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                    listView=findViewById(R.id.list);
+                    countrylist= getResources().getStringArray(R.array.country);
+                    adapter=new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,countrylist);
+                    listView.setAdapter(adapter);
+                    listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                         @Override
-                        public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                            String name = adapterView.getItemAtPosition(i).toString();
-                            if(i==0)
-                                Toast.makeText(getApplicationContext(), "Please Select a country", Toast.LENGTH_SHORT).show();
+                        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                            String name=adapterView.getItemAtPosition(i).toString();
+                            if(name.equals(adapterView.getItemAtPosition(0).toString()))
+                                Toast.makeText(getApplicationContext(), "SELECT A VALID CHOICE", Toast.LENGTH_LONG).show();
                             else
-                                showDialogMessage(name);
-                        }
-                        @Override
-                        public void onNothingSelected(AdapterView<?> adapterView) {
-                                result.setText("Select Your Country");
+                                Toast.makeText(getApplicationContext(), "YOU SELECTED "+ name.toUpperCase(), Toast.LENGTH_SHORT).show();
                         }
                     });
 
-                }
 
-    private void showDialogMessage(String name1) {
-        AlertDialog.Builder alertdialog=new AlertDialog.Builder(this);
-        alertdialog.setTitle("Confirm:").setMessage("You selected your country "+name1)
-                .setNegativeButton("No", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int i) {
-                        dialog.cancel();
-                    }
-                })
-                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int i) {
-                        result.setText(name1);
-                    }
-                }).show();
-        alertdialog.create();
+
+
     }
 }
