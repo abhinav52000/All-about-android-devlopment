87b0a119de1bb958cc773558e5196fcfa08d0289
Abhinav kumar <abhinhavkumar2000@gmail.com>
2022-01-22 20:49:24 +0530

    In this we have Snackbar mode of popup message in comment it have also detail for how it can be used as alternative of Toast

--- app/src/main/java/com/devveloper/myapplicationcode/MainActivity.java
+++ app/src/main/java/com/devveloper/myapplicationcode/MainActivity.java
@@ -1,6 +1,7 @@
 package com.devveloper.myapplicationcode;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.LinearLayoutCompat;
 
 import android.os.Bundle;
 import android.view.View;
@@ -10,13 +11,15 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.snackbar.Snackbar;
+
 
 public class MainActivity extends AppCompatActivity {
 
     TextView result;
     Spinner spinner;
     ArrayAdapter adapter;
-
+    LinearLayoutCompat linear;
                 @Override
                 protected void onCreate(Bundle savedInstanceState) {
                     super.onCreate(savedInstanceState);
@@ -27,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
                             android.R.layout.simple_spinner_item);
                     spinner=findViewById(R.id.spinnere);
                     result=findViewById(R.id.resulte);
+                    linear=findViewById(R.id.layoutme);
                     adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
                     spinner.setAdapter(adapter);
                     spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
@@ -34,7 +38,17 @@ public class MainActivity extends AppCompatActivity {
                         public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
                             String name= adapterView.getItemAtPosition(i).toString();
                             if(i==0)
-                                Toast.makeText(getApplicationContext(),"Select Country First",Toast.LENGTH_LONG).show();
+                            {
+                                Snackbar.make(linear,"Hey it seems you have not selected anything",Snackbar.LENGTH_INDEFINITE).setAction("x", new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View view) {
+
+                                    }
+                                }).show();
+                            }
+//                                Snackbar.make(linear, "Hey it seems you have not selected anything",Snackbar.LENGTH_LONG).show();
+//                                This is used inside if clause above when you are only doing it for small time or a bit more long time but you want to show your message for long then.
+//                            In short it is an alterantive of toast as well.
                             else
                                 result.setText(name);
                         }
